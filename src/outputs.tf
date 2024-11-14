f3f662a1f8cab76fe8c8f45e08e6de46a7da1f0d
Igor Rodionov <goruha@gmail.com>
2024-11-14 20:23:58 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,4 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "lf_tags" {
+  description = "List of LF tags created."
+  value       = module.lakeformation.lf_tags
 }
